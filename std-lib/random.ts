00e647ea31a160357135f02aaff812645897fd8f
Daniele Orlando <dnl.rlnd@gmail.com>
2020-04-29 18:10:25 +0200

    (std-lib) random module has better typing

--- std-lib/random.ts
+++ std-lib/random.ts
@@ -10,24 +10,24 @@ export function randomTimes(min: number, max: number) {
     return times(randomInt(min, max))
 }
 
-export function randomItem(list: Array<unknown>) {
-    return list[randomInt(0, list.length - 1)]
+export function randomItem<T>(list: Array<T>) {
+    const idx = randomInt(0, list.length - 1)
+    const item = list[idx]
+    return item
 }
 
-export function randomItems(list: Array<unknown>, min = 2, max?: number) {
-    const items = randomTimes(min, max
-        ? Math.min(max, list.length)
-        : list.length
-    )
-    .map(() => randomItem(list))
-    .reduce((list: Array<unknown>, item) => {
-        // Unique items.
-        if (! list.includes(item)) {
-            list.push(item)
+export function randomItems<T>(list: Array<T>, min = 2, max?: number) {
+    const count = Math.min(list.length, max ?? Number.MAX_SAFE_INTEGER)
+    const uniqueItems: Array<T> = []
+    const items = randomTimes(min, count).map(() => randomItem(list))
+
+    for (const item of items) {
+        if (uniqueItems.includes(item)) {
+            continue
         }
+        // Unique item.
+        uniqueItems.push(item)
+    }
 
-        return list
-    }, [])
-
-    return items
+    return uniqueItems
 }
