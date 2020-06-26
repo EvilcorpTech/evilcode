db5ce167c1029b6d9db4c8e6b129a0771b52e824
Daniele Orlando <dnl.rlnd@gmail.com>
2020-06-26 11:37:48 +0200

    (std-web) fetch-mock module has a random delayed fetch implementation, simulating slow network

--- std-web/fetch-mock.ts
+++ std-web/fetch-mock.ts
@@ -1,4 +1,8 @@
 import {createFetch, Fetch, FetchRequestMethod, FetchRequestOptions, JsonType} from './fetch'
+import {error, StdError} from '@eviljs/std-lib/error'
+import {randomInt} from '@eviljs/std-lib/random'
+
+export class MissingMock extends StdError {}
 
 /*
 * Mocks must be an object with the structure
@@ -29,7 +33,8 @@ export function mockFetch(fetch: Fetch, mocks: FetchMocks) {
                 const response = mockResponse(mocks, ...args)
 
                 if (! response) {
-                    throw `@eviljs/std-web/fetch-mock.mockFetch(): missing mock for '${args[0]}:${args[1]}'`
+                    const [method, path] = args
+                    return throwMissingMock(method, path)
                 }
 
                 return Promise.resolve(response)
@@ -55,6 +60,25 @@ export function mockFetch(fetch: Fetch, mocks: FetchMocks) {
     }
 
     return self
+}
+
+export function mockFetchDelayed(fetch: Fetch, mocks: FetchMocks, opts?: MockFetchDelayedOptions) {
+    const mockedFetch = mockFetch(fetch, mocks)
+    const {request} = mockedFetch
+    const minDelay = opts?.minDelay ?? 100 // 0.1 seconds.
+    const maxDelay = opts?.maxDelay ?? 2000 // 2 seconds.
+
+    // We replace the mocked Fetch.request() implementation with one simulating a slow network.
+    mockedFetch.request = function (...args) {
+        const delay = randomInt(minDelay, maxDelay)
+        const promise = wait(delay).then(() =>
+            // After have been waiting for a random time, we call the original API as is.
+            request.apply(mockedFetch, args)
+        )
+        return promise
+    }
+
+    return mockedFetch
 }
 
 export function mockResponse(mocks: FetchMocks, type: FetchRequestMethod, path: string, options?: FetchRequestOptions) {
@@ -90,6 +114,17 @@ export function jsonResponse(data: unknown, options?: FetchRequestOptions) {
     return response
 }
 
+export function wait(delay: number) {
+    return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
+export function throwMissingMock(method: string, path: string) {
+    const message =
+        `@eviljs/std-web/fetch-mock.mockFetch():\n`
+        + `missing mock for '${method}:${path}'.`
+    return error({type: MissingMock, message})
+}
+
 // Types ///////////////////////////////////////////////////////////////////////
 
 export type FetchMocks = {
@@ -99,3 +134,8 @@ export type FetchMocks = {
 export type FetchMethodMocks = Array<
     readonly [string, (options?: FetchRequestOptions) => Response]
 >
+
+export interface MockFetchDelayedOptions {
+    minDelay?: number
+    maxDelay?: number
+}
