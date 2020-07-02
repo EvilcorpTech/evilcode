c73aa63a3c54bc53c623fa52a9bc5319d8c2311c
Daniele Orlando <dnl.rlnd@gmail.com>
2020-07-02 13:41:36 +0200

    (std-react) has an auth barrier widget

---
+++ std-react/widgets/auth-barrier.tsx
@@ -0,0 +1,33 @@
+import {createElement} from 'react'
+import {useAuth, AuthTokenState} from '../auth'
+
+export function AuthBarrier(props: AuthBarrierProps) {
+    const {children, progress, fallback} = props
+    const {tokenState} = useAuth()
+
+    switch (tokenState) {
+        case AuthTokenState.Validating:
+            // We are waiting the response from the server.
+            return progress
+        break
+        case AuthTokenState.Valid:
+            // Token has been verified and is valid. We can safely continue.
+            return children
+        break
+        case AuthTokenState.Missing:
+        case AuthTokenState.Invalid:
+            // Token is missing or invalid.
+            return fallback
+        break
+    }
+
+    return null
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export interface AuthBarrierProps {
+    children: JSX.Element
+    progress: JSX.Element
+    fallback: JSX.Element
+}
