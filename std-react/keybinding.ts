5e9d3d0e981074fbeb7a53f24daae1b0de337fa5
Daniele Orlando <dnl.rlnd@gmail.com>
2020-12-02 15:30:44 +0100

    (std-react) keybinding hook can listen on any key
    * checkbox marker inherits color

--- std-react/keybinding.ts
+++ std-react/keybinding.ts
@@ -1,22 +1,52 @@
+import {asArray} from '@eviljs/std-lib/type.js'
 import React from 'react'
 const {useEffect} = React
 
-export function useEscKey(onKey: () => void) {
+export function useKey(key: Key, handler: KeyHandler, options?: UseKeyOptions) {
+    const keys = asArray(key)
+
     useEffect(() => {
-        function onEsc(event: KeyboardEvent) {
-            switch (event.key) {
-                case 'Escape':
-                    onKey()
-                break
+        const event = options?.event ?? 'keyup'
+        const phase = (() => {
+            switch (options?.phase) {
+                case 'capturing':
+                    return true
+                case 'bubbling':
+                    return false
+                default:
+                    return false // bubbling by default.
             }
+        })()
+
+        function onKey(event: KeyboardEvent) {
+            const isTheKey = keys.includes(event.key)
+
+            if (! isTheKey) {
+                return
+            }
+
+            handler(event)
         }
 
         function unmount() {
-            document.removeEventListener('keyup', onEsc, false)
+            document.removeEventListener(event, onKey, phase)
         }
 
-        document.addEventListener('keyup', onEsc, false)
+        document.addEventListener(event, onKey, phase)
 
         return unmount
-    }, [onKey])
+    }, [handler])
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export type Key = string | Array<string> // https://developer.mozilla.org/it/docs/Web/API/KeyboardEvent/key/Key_Values
+
+export interface KeyHandler {
+    (event: KeyboardEvent): void
+}
+
+export interface UseKeyOptions {
+    event?: 'keyup' | 'keydown'
+    phase?: 'capturing' | 'bubbling'
 }
