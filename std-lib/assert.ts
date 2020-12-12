b5fff676b53b8364e79dc4da29f47863d736f673
Daniele Orlando <dnl.rlnd@gmail.com>
2020-12-12 20:31:57 +0100

    (std-lib) assertions are generic

--- std-lib/assert.ts
+++ std-lib/assert.ts
@@ -3,7 +3,7 @@ import {isArray, isBoolean, isFunction, isInteger, isNumber, isObject, isString}
 import {isDate, isDateString, isDateIsoUtcString} from './type.js'
 import {isUndefined} from './type.js'
 
-export function assertOptionalWith<T>(assertion: Assertion<T>, value: unknown, ...args: Array<unknown>) {
+export function assertOptionalWith<T>(assertion: Assertion<T>, value: undefined | T, ...args: Array<unknown>) {
     if (isUndefined(value)) {
         return
     }
@@ -11,72 +11,72 @@ export function assertOptionalWith<T>(assertion: Assertion<T>, value: unknown, .
     return assertion(value, ...args)
 }
 
-export function assertArray(value: unknown, name?: string) {
+export function assertArray<T extends Array<unknown>>(value: T, ctx?: any) {
     if (isArray(value)) {
         return value
     }
 
-    return throwError('an Array', value, name)
+    return throwError('an Array', value, ctx)
 }
 
-export function assertArrayOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertArray, value, name)
+export function assertArrayOptional<T extends Array<unknown>>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertArray, value, ctx)
 }
 
-export function assertBoolean(value: unknown, name?: string) {
+export function assertBoolean<T extends boolean>(value: T, ctx?: any) {
     if (isBoolean(value)) {
         return value
     }
 
-    return throwError('a Boolean', value, name)
+    return throwError('a Boolean', value, ctx)
 }
 
-export function assertBooleanOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertBoolean, value, name)
+export function assertBooleanOptional<T extends boolean>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertBoolean, value, ctx)
 }
 
-export function assertDate(value: unknown, name?: string) {
+export function assertDate<T extends Date>(value: T, ctx?: any) {
     if (isDate(value)) {
         return value
     }
 
-    return throwError('a Date', value, name)
+    return throwError('a Date', value, ctx)
 }
 
-export function assertDateOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertDate, value, name)
+export function assertDateOptional<T extends Date>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertDate, value, ctx)
 }
 
-export function assertDateString(value: unknown, name?: string) {
-    assertStringNotEmpty(value, name)
+export function assertDateString<T extends string>(value: T, ctx?: any) {
+    assertStringNotEmpty(value, ctx)
 
     if (isDateString(value)) {
         return value
     }
 
-    return throwError('a Date string', value, name)
+    return throwError('a Date string', value, ctx)
 }
 
-export function assertDateStringOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertDateString, value, name)
+export function assertDateStringOptional<T extends string>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertDateString, value, ctx)
 }
 
-export function assertDateAsIsoUtcString(value: unknown, name?: string) {
-    assertDateString(value, name)
+export function assertDateAsIsoUtcString<T extends string>(value: T, ctx?: any) {
+    assertDateString(value, ctx)
 
     if (isDateIsoUtcString(value)) {
         return value
     }
 
-    return throwError('a Date as ISO string with UTC timezone', value, name)
+    return throwError('a Date as ISO string with UTC timezone', value, ctx)
 }
 
-export function assertDateAsIsoUtcStringOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertDateAsIsoUtcString, value, name)
+export function assertDateAsIsoUtcStringOptional<T extends string>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertDateAsIsoUtcString, value, ctx)
 }
 
-export function assertEnum(value: unknown, enumValues: Array<unknown>, name?: string) {
-    assertArray(enumValues, `${name} enum`)
+export function assertEnum<E, T extends E>(value: T, enumValues: Array<E>, ctx?: any) {
+    assertArray(enumValues, `${ctx} enum`)
 
     for (const enumValue of enumValues) {
         if (value === enumValue) {
@@ -84,109 +84,112 @@ export function assertEnum(value: unknown, enumValues: Array<unknown>, name?: st
         }
     }
 
-    return throwError(`one of ${enumValues.join(' | ')}`, value, name)
+    return throwError(`one of ${enumValues.join(' | ')}`, value, ctx)
 }
 
-export function assertEnumOptional(value: unknown, enumValues: Array<unknown>, name?: string) {
-    return assertOptionalWith(assertEnum, value, enumValues, name)
+export function assertEnumOptional<E, T extends E>(value: undefined | T, enumValues: Array<E>, ctx?: any) {
+    return assertOptionalWith(assertEnum, value, enumValues, ctx)
 }
 
-export function assertFunction(value: unknown, name?: string) {
+export function assertFunction<T extends Function>(value: T, ctx?: any) {
     if (isFunction(value)) {
         return value
     }
 
-    return throwError('a Function', value, name)
+    return throwError('a Function', value, ctx)
 }
 
-export function assertFunctionOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertFunction, value, name)
+export function assertFunctionOptional<T extends Function>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertFunction, value, ctx)
 }
 
-export function assertInteger(value: unknown, name?: string) {
-    assertNumber(value, name)
+export function assertInteger<T extends number>(value: T, ctx?: any) {
+    assertNumber(value, ctx)
 
     if (isInteger(value)) {
         return value
     }
 
-    return throwError('an Integer', value, name)
+    return throwError('an Integer', value, ctx)
 }
 
-export function assertIntegerOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertInteger, value, name)
+export function assertIntegerOptional<T extends number>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertInteger, value, ctx)
 }
 
-export function assertNumber(value: unknown, name?: string) {
+export function assertNumber<T extends number>(value: T, ctx?: any) {
     if (isNumber(value)) {
         return value
     }
 
-    return throwError('a Number', value, name)
+    return throwError('a Number', value, ctx)
 }
 
-export function assertNumberOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertNumber, value, name)
+export function assertNumberOptional<T extends number>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertNumber, value, ctx)
 }
 
-export function assertObject(value: unknown, name?: string) {
+export function assertObject<T extends {} | Record<PropertyKey, unknown>>(value: T, ctx?: any) {
     if (isObject(value)) {
         return value
     }
 
-    return throwError('an Object', value, name)
+    return throwError('an Object', value, ctx)
 }
 
-export function assertObjectOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertObject, value, name)
+export function assertObjectOptional<T extends {} | Record<PropertyKey, unknown>>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertObject, value, ctx)
 }
 
-export function assertString(value: unknown, name?: string) {
+export function assertString<T extends string>(value: T, ctx?: any) {
     if (isString(value)) {
         return value
     }
 
-    return throwError('a String', value, name)
+    return throwError('a String', value, ctx)
 }
 
-export function assertStringOptional(value: unknown, name?: string) {
+export function assertStringOptional<T extends string>(value: undefined | T, ctx?: any) {
     if (value === '') {
         return value
     }
 
-    return assertOptionalWith(assertString, value, value, name)
+    return assertOptionalWith(assertString, value, value, ctx)
 }
 
-export function assertStringNotEmpty(value: unknown, name?: string) {
-    assertString(value, name)
+export function assertStringNotEmpty<T extends string>(value: T, ctx?: any) {
+    assertString(value, ctx)
 
     if (isString(value) && value.trim() !== '') {
         return value
     }
 
-    return throwError('a not empty String', value, name)
+    return throwError('a not empty String', value, ctx)
 }
 
-export function assertStringNotEmptyOptional(value: unknown, name?: string) {
-    return assertOptionalWith(assertStringNotEmpty, value, name)
+export function assertStringNotEmptyOptional<T extends string>(value: undefined | T, ctx?: any) {
+    return assertOptionalWith(assertStringNotEmpty, value, ctx)
 }
 
-export function assertUndefined(value: unknown, name?: string) {
+export function assertUndefined<T extends undefined>(value: T, ctx?: any) {
     if (value === void undefined) {
         return
     }
 
-    return throwError('undefined', value, name)
+    return throwError('undefined', value, ctx)
 }
 
-export function errorMessage(type: string, value: unknown, name?: string) {
-    return `${name ? `"${name}"` : 'value'} must be ${type}, given "${value}".`
+export function errorMessage(expected: string, actual: unknown, ctx?: any) {
+    const who = ctx
+        ? `"${ctx}"`
+        : 'value'
+    return `${who} must be ${expected}, given "${actual}".`
 }
 
-export function throwError(type: string, value: unknown, name?: string) {
-    return throwInvalidArgument(errorMessage(type, value, name))
+export function throwError(type: string, value: unknown, ctx?: any) {
+    return throwInvalidArgument(errorMessage(type, value, ctx))
 }
 
 // Types ///////////////////////////////////////////////////////////////////////
 
-export type Assertion<T> = (value: unknown, ...args: Array<any>) => T
+export type Assertion<T> = (value: T, ...args: Array<any>) => T
