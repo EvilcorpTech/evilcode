7c2b088c1a72fecf8866c06cb641ee4c1040105a
Daniele Orlando <dnl.rlnd@gmail.com>
2021-07-20 15:24:33 +0200

    (web) has an element module supporting custom elements

---
+++ packages/web/element.ts
@@ -0,0 +1,80 @@
+const WebElementState = Symbol('WebElementState')
+
+export class WebElement extends HTMLElement {
+    [WebElementState]: {
+        mounted: Array<Function>
+        unmounted: Array<Function>
+    }
+
+    constructor() {
+        super()
+
+        this[WebElementState] = {
+            mounted: [],
+            unmounted: [],
+        }
+    }
+
+    connectedCallback() {
+        flushTasksQueue(this[WebElementState].mounted)
+    }
+
+    disconnectedCallback() {
+        this[WebElementState].unmounted = flushTasksQueue(this[WebElementState].unmounted)
+    }
+}
+
+// Lifecycles //////////////////////////////////////////////////////////////////
+
+export function onMounted(element: WebElement, fn: OnMountedFn): void {
+    const unmountedTask = fn()
+
+    if (! unmountedTask) {
+        return
+    }
+
+    element[WebElementState].unmounted.push(unmountedTask)
+}
+
+export function onUnmounted(element: WebElement, fn: OnUnmountedFn): void {
+    onMounted(element, () => fn)
+}
+
+// Hooks ///////////////////////////////////////////////////////////////////////
+
+export function useEventListener<T extends keyof WindowEventMap>(a: WebElement, b: Window, c: T, d: (e: WindowEventMap[T]) => any, e?: boolean | AddEventListenerOptions): void
+export function useEventListener<T extends keyof DocumentEventMap>(a: WebElement, b: Document, c: T, d: (e: DocumentEventMap[T]) => any, e?: boolean | AddEventListenerOptions): void
+export function useEventListener<T extends keyof HTMLElementEventMap>(a: WebElement, b: HTMLElement, c: T, d: (e: HTMLElementEventMap[T]) => any, e?: boolean | AddEventListenerOptions): void
+export function useEventListener<T extends keyof SVGElementEventMap>(a: WebElement, b: SVGElement, c: T, d: (e: SVGElementEventMap[T]) => any, e?: boolean | AddEventListenerOptions): void
+export function useEventListener(a: WebElement, b: Element, c: string, d: EventListener, e?: boolean | AddEventListenerOptions): void
+export function useEventListener(element: WebElement, target: Window | Document | Element, type: string, listener: EventListener, options?: boolean | AddEventListenerOptions): void {
+    onMounted(element, () => {
+        target.addEventListener(type, listener, options)
+
+        function unmount() {
+            target.removeEventListener(type, listener, options)
+        }
+
+        return unmount
+    })
+}
+
+// Tools ///////////////////////////////////////////////////////////////////////
+
+export function flushTasksQueue(queue: Array<Function>): Array<Function> {
+    for (const it of queue) {
+        it()
+    }
+
+    return []
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export interface OnMountedFn {
+    (): Function | void
+}
+
+export interface OnUnmountedFn {
+    (): void
+}
