db72461532a86ffe1242740e75364758d15bc822
Daniele Orlando <dnl.rlnd@gmail.com>
2021-09-12 14:26:14 +0200

    (eviljs-reactx) reworks armored button

--- packages/eviljs-reactx/button-armored/index.tsx
+++ packages/eviljs-reactx/button-armored/index.tsx
@@ -1,52 +1,38 @@
 import {classes} from '@eviljs/react/react.js'
-import {useState} from 'react'
-import {Button} from '../button/index.js'
-
-import './index.css'
+import {useCallback, useState} from 'react'
 
 export function ArmoredButton(props: ArmoredButtonProps) {
-    const {armorClass, cancel, cancelClass, children, className, confirm, confirmClass, onClick, ...otherProps} = props
+    const {children, className, ...otherProps} = props
     const [armored, setArmored] = useState(false)
+
+    const armor = useCallback(() => {
+        setArmored(true)
+    }, [])
+    const close = useCallback(() => {
+        setArmored(false)
+    }, [])
 
     return (
         <div
             {...otherProps}
-            className={classes('ArmoredButton-aa3e std-flex align-center', className, {armored})}
+            className={classes('ArmoredButton-aa3e std-flex align-center', className, {
+                initial: ! armored,
+                armored,
+            })}
         >
-            <Button
-                className={classes('armor-eb3d', armorClass)}
-                onClick={() => setArmored(true)}
-            >
-                {children ?? 'Armor'}
-            </Button>
-
-            <Button
-                className={classes('cancel-da15', cancelClass)}
-                onClick={() => setArmored(false)}
-            >
-                {cancel ?? 'Cancel'}
-            </Button>
-
-            <Button
-                className={classes('confirm-eccb', confirmClass)}
-                onClick={(event) => {
-                    setArmored(false)
-                    onClick?.(event)
-                }}
-            >
-                {confirm ?? 'Confirm'}
-            </Button>
+            {children({isArmored: armored, armor, close})}
         </div>
     )
 }
 
 // Types ///////////////////////////////////////////////////////////////////////
 
-export interface ArmoredButtonProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onClick'> {
-    armorClass?: string
-    cancel?: React.ReactNode
-    cancelClass?: string
-    confirm?: React.ReactNode
-    confirmClass?: string
-    onClick?(event: React.MouseEvent<HTMLButtonElement>): void
+export interface ArmoredButtonProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+    children(props: ArmoredChildrenProps): React.ReactNode
+}
+
+export interface ArmoredChildrenProps {
+    isArmored: boolean
+    armor(): void
+    close(): void
 }
