43fd97e7451e97cd1f4645b9f02646586231567a
Daniele Orlando <dnl.rlnd@gmail.com>
2021-11-11 02:45:18 +0100

    (eviljs-std) async module can play async timelines

--- packages/eviljs-std/async.ts
+++ packages/eviljs-std/async.ts
@@ -1,7 +1,54 @@
+import {isArray, isFunction, isObject} from './type.js'
+import {throwInvalidArgument} from './throw.js'
+
 export function wait(delay: number) {
     const promise = new Promise((resolve) =>
         setTimeout(resolve, delay)
     )
 
     return promise
+}
+
+export async function play(timeline: AsyncTimeline): Promise<unknown> {
+    if (isFunction(timeline)) {
+        return await timeline()
+    }
+    if (isArray(timeline)) {
+        return await Promise.all(timeline.map(play))
+    }
+    if (isObject(timeline)) {
+        return await Promise.all(Object.values(timeline).map(play))
+    }
+
+    return throwInvalidArgument(
+        '@eviljs/std/async.play(~~timeline~~):\n'
+        + `timeline must be a Function | Object | Array, given "${timeline}".`
+    )
+}
+
+export class PromiseCancellable<T = unknown> extends Promise<T> {
+    cancelled: boolean = false
+
+    cancel() {
+        this.cancelled = true
+    }
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export type AsyncTimeline<R = unknown> =
+    | AsyncTimelineTask<R>
+    | AsyncTimelineSequence<R>
+    | AsyncTimelineParallel<R>
+    | Readonly<AsyncTimelineParallel<R>>
+
+export interface AsyncTimelineTask<R = unknown> {
+    (): Promise<R>
+}
+
+export interface AsyncTimelineSequence<R = unknown> extends Array<AsyncTimeline<R>> {
+}
+
+export interface AsyncTimelineParallel<R = unknown> {
+    [key: PropertyKey]: AsyncTimeline<R>
 }
