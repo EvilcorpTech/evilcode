f60e3a8b98a9b9e738b4395bdac8a6c6011a3f1a
Daniele Orlando <dnl.rlnd@gmail.com>
2022-01-07 10:51:33 +0100

    (eviljs-web) ssr uses provided key/id

--- packages/eviljs-web/ssr.ts
+++ packages/eviljs-web/ssr.ts
@@ -1,22 +1,17 @@
-export const SsrIdPrefix = 'std-ssr-'
-
 export function saveSsrState(id: string, payload: any, options?: SsrOptions) {
     if (! payload) {
         return
     }
 
-    const ssrId = formatSsrId(id, options)
     const serializedPayload = JSON.stringify(payload)
     const inject = options?.inject ?? injectSsrStorageElement
-    const ssrStorage = findSsrStorageElement(ssrId) ?? inject()
+    const ssrStorage = findSsrStorageElement(id) ?? inject(id)
 
-    ssrStorage.id = ssrId
     ssrStorage.textContent = serializedPayload
 }
 
 export function loadSsrState<S = unknown>(id: string, options?: SsrOptions) {
-    const ssrId = formatSsrId(id, options)
-    const ssrStorage = findSsrStorageElement(ssrId)
+    const ssrStorage = findSsrStorageElement(id)
     const serializedPayload = ssrStorage?.textContent?.trim()
 
     if (! serializedPayload) {
@@ -26,27 +21,23 @@ export function loadSsrState<S = unknown>(id: string, options?: SsrOptions) {
     return JSON.parse(serializedPayload) as S
 }
 
-export function formatSsrId(id: string, options?: SsrOptions) {
-    return options?.formatId
-        ? options.formatId(id)
-        : SsrIdPrefix + id
-}
-
 export function findSsrStorageElement(id: string) {
     const selector = '#' + id
     return document.querySelector(selector)
 }
 
-export function injectSsrStorageElement() {
+export function injectSsrStorageElement(id: string) {
     const el = document.createElement('script')
+    el.id = id
     el.type = 'application/json'
+
     document.body.appendChild(el)
+
     return el
 }
 
 // Types ///////////////////////////////////////////////////////////////////////
 
 export interface SsrOptions {
-    formatId?(id: string): string
-    inject?(): Element
+    inject?(id: string): Element
 }
