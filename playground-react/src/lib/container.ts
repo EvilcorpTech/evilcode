32a3fa7cb1fda896f783b2f8bcfa66742d45d919
Daniele Orlando <dnl.rlnd@gmail.com>
2022-01-07 10:55:30 +0100

    (playground-react) aligns with the upstream

--- playground-react/src/lib/container.ts
+++ playground-react/src/lib/container.ts
@@ -3,10 +3,11 @@ import {createI18n} from '@eviljs/std/i18n'
 import {createLogger} from '@eviljs/std/logger'
 import {createCookie, maxAgeInDays} from '@eviljs/web/cookie'
 import {asBaseUrl, createFetch, Fetch} from '@eviljs/web/fetch'
+import {mockFetchDelayed} from '@eviljs/web/fetch-mock'
 import {createQuery} from '@eviljs/web/query'
 import {ApiUrl, BasePath, BundleName} from './context'
 import {I18nSpec} from './i18n'
-import {mockFetch} from './mock'
+import {FetchMocksSpec} from './mock'
 import {StoreSpec} from './store'
 
 export const ContainerSpec = {
@@ -23,8 +24,8 @@ export const ContainerSpec = {
             // navigator.serviceWorker.register(serviceWorkerUrl)
             // return fetch
 
-            // PROXY ///////////////////////////////////////////////////////////
-            return mockFetch(fetch)
+            // MOCK ////////////////////////////////////////////////////////////
+            return mockFetchDelayed(fetch, FetchMocksSpec, {minDelay: 500, maxDelay: 1000})
 
         },
         I18n(container: {}) {
