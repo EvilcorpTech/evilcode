28ea16d2de2923121e3f3cd55efd5f484f54afd8
Daniele Orlando <dnl.rlnd@gmail.com>
2022-01-27 01:49:52 +0100

    (eviljs-reactx) has HtmlSandbox, Showcase and Site

---
+++ packages/eviljs-reactx/html-sandbox.tsx
@@ -0,0 +1,29 @@
+import {classes} from '@eviljs/react/react.js'
+import {HtmlSandbox as HtmlSandboxElement} from '@eviljs/webx/html-sandbox.js'
+import {createElement} from 'react'
+
+export const HtmlSandboxTag = 'html-sandbox'
+
+export function HtmlSandbox(props: HtmlSandboxProps) {
+    const {children, className, tag, ...otherProps} = props
+
+    return (
+        createElement(tag ?? HtmlSandboxTag, {
+            ...otherProps,
+            className: classes('html-sandbox-d25f', className),
+        }, children)
+    )
+}
+
+export function defineHtmlSandbox(tag = HtmlSandboxTag) {
+    if (! customElements.get(tag)) { // HMR Compatibility.
+        customElements.define(tag, HtmlSandboxElement)
+    }
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export interface HtmlSandboxProps extends React.HTMLAttributes<HTMLElement> {
+    tag?: undefined | string
+    children: string
+}
---
+++ packages/eviljs-reactx/showcase.tsx
@@ -0,0 +1,93 @@
+import {Box, BoxProps} from '@eviljs/react/box.js'
+import {classes} from '@eviljs/react/react.js'
+import {Route, useRouter} from '@eviljs/react/router.js'
+import {createElement, useEffect, useMemo, useState} from 'react'
+
+export function Showcase(props: ShowcaseProps) {
+    const {children, className, ...otherProps} = props
+    const [selected, setSelected] = useState('')
+    const [search, setSearch] = useState('')
+    const router = useRouter()
+    const {replaceRoute, routeParams, routePath} = router
+
+    useEffect(() => {
+        const defaultShowcase = children[0]
+        const defaultId = defaultShowcase
+            ? idForShowcase(defaultShowcase)
+            : ''
+
+        setSelected(routeParams.id ?? defaultId)
+    }, [routeParams.id])
+
+    useEffect(() => {
+        setSearch(routeParams.search ?? '')
+    }, [routeParams.search])
+
+    const items = useMemo(() => {
+        const matchingItems = children.filter(it =>
+            it[0].toLowerCase().includes(search.toLowerCase())
+        )
+        return matchingItems
+    }, [search])
+
+    const selectedItem = children.find(it => idForShowcase(it) === selected)
+        ?? children[0]
+    const selectedComponent = selectedItem?.[1]
+
+    return (
+        <Box
+            {...otherProps}
+            className={classes('Showcase-d1c8', className)}
+        >
+            <nav className="navigation-70eb std-shadow z2">
+                <input
+                    className="input-0caa std-text-body2"
+                    value={search}
+                    placeholder="Search something..."
+                    onChange={event =>
+                        replaceRoute(routePath, {
+                            ...routeParams,
+                            search: event.target.value ?? '',
+                        })
+                    }
+                />
+
+                <i className="std-space-v s4"/>
+
+                {items.map((it, idx) =>
+                    <Route
+                        key={idx}
+                        className={classes('item-988a', {
+                            selected: idForShowcase(it) === selected,
+                        })}
+                        to={routePath}
+                        params={{...routeParams, id: idForShowcase(it)}}
+                    >
+                        {it[0]}
+                    </Route>
+                )}
+            </nav>
+
+            <div className="workspace-a486">
+                {selectedComponent && createElement(selectedComponent)}
+            </div>
+        </Box>
+    )
+}
+
+export function defineShowcase(title: string, component: React.ComponentType): ShowcaseModel {
+    return [title, component]
+}
+
+export function idForShowcase(item: ShowcaseModel) {
+    const [title] = item
+    return title
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export interface ShowcaseProps extends BoxProps {
+    children: Array<ShowcaseModel>
+}
+
+export type ShowcaseModel = [string, React.ComponentType]
