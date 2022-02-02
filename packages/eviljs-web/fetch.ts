7c90ead50dbb0fcd6031c7c3bcf1c5cc57228b34
Daniele Orlando <dnl.rlnd@gmail.com>
2022-02-03 00:31:12 +0100

    (eviljs-web) fetch provides a joinPath() function

--- packages/eviljs-web/fetch.ts
+++ packages/eviljs-web/fetch.ts
@@ -68,6 +68,42 @@ export function asBaseUrl(url?: undefined | string) {
     return url
 }
 
+// const joinPathTests: Array<[string, [string, string, ...Array<string>]]> = [
+//     ['/api', ['', 'api']],
+//     ['/api', ['', '/api']],
+//     ['/api/', ['/', 'api/']],
+//     ['/api/', ['/', '/api/']],
+//     ['api/v1', ['api', 'v1']],
+//     ['/api/v1', ['/api', 'v1']],
+//     ['/api/v1', ['/api', '/v1']],
+//     ['/api/v1', ['/api/', '/v1']],
+//     ['/api/v1/id/', ['/api/', '/v1/', '/id/']],
+//     ['https://api.com/api', ['https://api.com', 'api']],
+//     ['https://api.com/api', ['https://api.com/', 'api']],
+//     ['https://api.com/api', ['https://api.com', '/api']],
+//     ['https://api.com/api', ['https://api.com/', '/api']],
+//     ['https://api.com/api/v2', ['https://api.com', '/api/', 'v2']],
+// ]
+// for (const it of joinPathTests) {
+//     const [expected, args] = it
+//     const actual = joinPath(...args)
+//     console.assert(expected === actual, 'expected:', expected, 'given:', actual, args)
+// }
+export function joinPath(firstPart: string, secondPart: string, ...otherParts: Array<string>) {
+    const parts = [secondPart, ...otherParts]
+
+    let path = firstPart
+    for (const it of parts) {
+        if (! path.endsWith('/')) {
+            path += '/'
+        }
+        path += it.startsWith('/')
+            ? it.slice(1)
+            : it
+    }
+    return path
+}
+
 export function mergeOptions(...optionsList: Array<FetchRequestOptions>): FetchRequestOptions {
     type Options = Omit<FetchRequestOptions, 'headers'> & {headers: Record<string, string>}
     const options: Options = {
