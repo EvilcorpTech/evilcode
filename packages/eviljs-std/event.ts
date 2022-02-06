20995001fdd94e3662a19c16302df06e458bbb8b
Daniele Orlando <dnl.rlnd@gmail.com>
2022-02-06 17:28:04 +0100

    (eviljs-std) provides the event module with debounce and throttle

---
+++ packages/eviljs-std/event.ts
@@ -0,0 +1,93 @@
+export function debounce<A extends Array<unknown>>(task: Task<A>, delay: number) {
+    interface State {
+        args: null | A
+        callTime: null | number
+        timeoutId: null | number
+    }
+    const state: State = {
+        args: null,
+        callTime: null,
+        timeoutId: null,
+    }
+
+    function call(...args: A) {
+        state.args = args
+        state.callTime = Date.now()
+
+        if (state.timeoutId !== null) {
+            return
+        }
+
+        state.timeoutId = setTimeout(run, delay)
+    }
+
+    function cancel() {
+        if (state.timeoutId === null) {
+            return
+        }
+
+        clearTimeout(state.timeoutId)
+    }
+
+    function run() {
+        const elapsedTime = Date.now() - state.callTime!
+        const isTimeExpired = elapsedTime >= delay
+
+        if (isTimeExpired) {
+            state.timeoutId = null
+            task(...state.args!)
+        }
+        else {
+            const remainingDelay = delay - elapsedTime
+            state.timeoutId = setTimeout(run, remainingDelay)
+        }
+    }
+
+    call.cancel = cancel
+
+    return call
+}
+
+export function throttle<A extends Array<unknown>>(task: Task<A>, delay: number) {
+    interface State {
+        args: null | A
+        timeoutId: null | number
+    }
+    const state: State = {
+        args: null,
+        timeoutId: null,
+    }
+
+    function call(...args: A) {
+        state.args = args
+
+        if (state.timeoutId !== null) {
+            return
+        }
+
+        state.timeoutId = setTimeout(run, delay)
+    }
+
+    function cancel() {
+        if (state.timeoutId === null) {
+            return
+        }
+
+        clearTimeout(state.timeoutId)
+    }
+
+    function run() {
+        state.timeoutId = null
+        task(...state.args!)
+    }
+
+    call.cancel = cancel
+
+    return call
+}
+
+// Types ///////////////////////////////////////////////////////////////////////
+
+export interface Task<A extends Array<unknown>> {
+    (...args: A): void
+}
