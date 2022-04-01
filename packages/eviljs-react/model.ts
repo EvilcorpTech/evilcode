65594ffd5514cd7d88bf95eee98266d0ac302274
Daniele Orlando <dnl.rlnd@gmail.com>
2022-04-01 12:15:57 +0200

    (eviljs-react) improves model typing

--- packages/eviljs-react/model.ts
+++ packages/eviljs-react/model.ts
@@ -2,13 +2,13 @@ export const NoItems: [] = []
 
 // Types ///////////////////////////////////////////////////////////////////////
 
-export interface ValueMutator<V, C = V> {
+export interface ValueMutator<V, C = V, R = void> {
     value: V
-    onChange: (value: C) => void
+    onChange: (value: C) => R
 }
 
-export interface ItemSelector<I, V, C = V> {
+export interface ItemSelector<I, V, C = V, R = void> {
     items: Array<I>
     selected: V
-    onSelect: (value: C, idx: number) => void
+    onSelect: (value: C, idx: number) => R
 }
