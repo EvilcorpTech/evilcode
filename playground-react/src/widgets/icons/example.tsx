fbae8e92294dffa3bbad6e8ee2832e7c50236ce2
Daniele Orlando <dnl.rlnd@gmail.com>
2022-10-19 19:58:45 +0200

    (playground-react) aligns with upstream

--- playground-react/src/widgets/icons/example.tsx
+++ playground-react/src/widgets/icons/example.tsx
@@ -1,19 +1,19 @@
-import {defineIcon} from '@eviljs/react/icon'
+import {defineSvg} from '@eviljs/react/svg'
 
-export const ExampleIcon = defineIcon({
+export const ExampleIcon = defineSvg({
     name: 'ExampleIcon',
     className: 'std-icon',
     viewBox: '0 0 512 512',
     children: <>
         <path
             fill="none"
-            strokeWidth="var(--stroke-width, 2)"
+            strokeWidth="var(--stroke-size, 2)"
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z"
         />
         <path
-            strokeWidth="var(--stroke-width, 2)"
+            strokeWidth="var(--stroke-size, 2)"
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M17.5 17.5L13.875 13.875"
