99586f3b84ed53a181ba50c9b953c53cd90799d5
Daniele Orlando <dnl.rlnd@gmail.com>
2022-11-23 19:15:36 +0100

    (evilcode) removes deprecated and unused packages

--- site/src/lib/routes.ts
+++
@@ -1,5 +0,0 @@
-import {Arg, createRoute, createSimpleRoute} from '@eviljs/web/route'
-
-export const HomeRoute = createSimpleRoute('/')
-export const ThemeRoute = createSimpleRoute('/theme')
-export const DocRoute = createSimpleRoute('/doc/' + Arg)
--- site/src/main.ts
+++
@@ -1,7 +0,0 @@
-import {createApp} from 'vue'
-import {App} from './app/App'
-
-import './lib/theme.css'
-
-const app = createApp(App)
-app.mount('#App')
