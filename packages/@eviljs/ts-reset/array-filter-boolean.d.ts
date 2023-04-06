b802b78fee93e19f7678b1f1bc5fbcf61f239245
Daniele Orlando <dnl.rlnd@gmail.com>
2023-04-06 16:25:19 +0200

    (@eviljs/ts-reset) fixes wrong Array.filter typing

--- packages/@eviljs/ts-reset/array-filter-boolean.d.ts
+++ packages/@eviljs/ts-reset/array-filter-boolean.d.ts
@@ -1,11 +1,15 @@
 interface Array<T> {
-    filter(predicate: BooleanConstructor, thisArg?: any): T extends undefined | null | false | 0 | 0n | ''
-        ? never
-        : T
+    filter(predicate: BooleanConstructor, thisArg?: any): Array<
+        T extends undefined | null | false | 0 | 0n | ''
+            ? never
+            : T
+    >
 }
 
 interface ReadonlyArray<T> {
-    filter(predicate: BooleanConstructor, thisArg?: any): T extends undefined | null | false | 0 | 0n | ''
-        ? never
-        : T
+    filter(predicate: BooleanConstructor, thisArg?: any): Array<
+        T extends undefined | null | false | 0 | 0n | ''
+            ? never
+            : T
+    >
 }
