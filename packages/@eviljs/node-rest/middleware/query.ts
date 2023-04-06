bb89be74c48c1159a02f19aed02ab36f9d23449c
Daniele Orlando <dnl.rlnd@gmail.com>
2023-04-06 19:27:04 +0200

    (@eviljs/node-rest) decodes unsafe JSON.parse() output

--- packages/@eviljs/node-rest/middleware/query.ts
+++ packages/@eviljs/node-rest/middleware/query.ts
@@ -1,17 +1,17 @@
+import {ensureArray} from '@eviljs/std/assert.js'
 import type {Context, Next} from 'koa'
 
 export const QueryRulesHeader = 'X-Query'
 
 export function queryMiddleware(context: Context, next: Next) {
     const {request} = context
-    const rules = rulesFromRequest(context)
 
-    request.rules = rules
+    request.rules = rulesFromRequest(context)
 
     return next()
 }
 
-export function rulesFromRequest(context: Context) {
+export function rulesFromRequest(context: Context): undefined | Array<string> {
     const rulesRaw = context.get(QueryRulesHeader)
     const rulesString = rulesRaw?.trim().toLowerCase()
 
@@ -20,17 +20,18 @@ export function rulesFromRequest(context: Context) {
     }
 
     try {
-        const rules = JSON.parse(rulesString)
-        return rules
+        return ensureArray(JSON.parse(rulesString) as unknown).map(String)
     }
     catch (error) {
     }
+
+    return // Makes TypeScript happy.
 }
 
 // Types ///////////////////////////////////////////////////////////////////////
 
 declare module 'koa' {
     interface Request {
-        rules?: Array<string>
+        rules?: undefined | Array<string>
     }
 }
