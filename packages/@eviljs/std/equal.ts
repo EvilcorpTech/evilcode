becd453789753db64a75c11d4b7f8903de8a0e01
Daniele Orlando <dnl.rlnd@gmail.com>
2023-04-07 21:28:44 +0200

    (@eviljs/std) equal name reflects the adopted strategy

--- packages/@eviljs/std/equal.ts
+++ packages/@eviljs/std/equal.ts
@@ -2,7 +2,7 @@ export function areEqualIdentity<T>(a: T, b: T) {
     return a === b
 }
 
-export function areEqualDeep(a: any, b: any) {
+export function areEqualDeepSerializable(a: any, b: any) {
     const aString = JSON.stringify(a)
     const bString = JSON.stringify(b)
     return aString === bString
