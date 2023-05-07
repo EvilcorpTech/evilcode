5df9e5e375af328f0ac21e5b7f3b714cc76cfe2b
Daniele Orlando <dnl.rlnd@gmail.com>
2023-05-07 17:48:09 +0200

    (@eviljs/react) fixes a ref mergin api bug

--- packages/@eviljs/react/ref.ts
+++ packages/@eviljs/react/ref.ts
@@ -1,14 +1,16 @@
-import {isFunction} from '@eviljs/std/type.js'
+import {isFunction, isNil, type Writable} from '@eviljs/std/type.js'
 
 export function mergingRefs<T = any>(...refs: Array<undefined | Ref<T>>) {
     function refProxy(el: T) {
         for (const ref of refs) {
+            if (isNil(ref)) {
+                continue
+            }
             if (isFunction(ref)) {
                 ref(el)
+                continue
             }
-            else {
-                ;(ref as React.MutableRefObject<null | T>).current = el
-            }
+            ;(ref as Writable<typeof ref>).current = el
         }
     }
     return refProxy
