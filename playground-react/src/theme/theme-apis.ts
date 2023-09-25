a746b20a15ee3ac2d453f8721291ac70dc4e1681
Daniele Orlando <dnl.rlnd@gmail.com>
2023-09-25 09:02:53 +0200

    (playground-react) has a theme match api

--- playground-react/src/theme/theme-apis.ts
+++ playground-react/src/theme/theme-apis.ts
@@ -6,3 +6,12 @@ export enum Theme {
 export function themeClassesOf(theme: Theme): Array<string> {
     return ['std-root', `std-theme-${theme}`, 'std-text', 'std-color-theme']
 }
+
+export function whenTheme<D, L>(theme: Theme, matches: {dark: D, light: L}): D | L {
+    switch (theme) {
+        case Theme.Dark:
+            return matches.dark
+        case Theme.Light:
+            return matches.light
+    }
+}
