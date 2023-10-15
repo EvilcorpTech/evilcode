54353d27ef1be6be69d3bfa6754e13ee652a2af7
Daniele Orlando <dnl.rlnd@gmail.com>
2023-10-15 19:44:31 +0200

    (@eviljs/web) keybinding supports arrow keys

--- packages/@eviljs/web/keybinding.ts
+++ packages/@eviljs/web/keybinding.ts
@@ -1,5 +1,9 @@
 export enum KeyboardKey {
     Alt = 'Alt',
+    ArrowDown = 'ArrowDown',
+    ArrowLeft = 'ArrowLeft',
+    ArrowRight = 'ArrowRight',
+    ArrowUp = 'ArrowUp',
     Backspace = 'Backspace',
     Control = 'Control',
     Enter = 'Enter',
