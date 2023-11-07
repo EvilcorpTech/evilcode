a2c14e8ec59c1a7a6cae5aecc6b0453d2f31a44e
Daniele Orlando <dnl.rlnd@gmail.com>
2023-11-07 14:15:08 +0100

    (@eviljs/std) accessor supports async accessors

--- packages/@eviljs/std/accessor.ts
+++ packages/@eviljs/std/accessor.ts
@@ -1,11 +1,13 @@
-export function createAccessor<V>(read: () => V, write: (value: V) => V): AccessorSync<V> {
-    // We opted out from using a `.value` getter+setter,
-    // because getter and setters can not be spread using the spread operator (...)
-    // and Object.assign() but requires a special extend utility.
-    // It would be an easy trap for the API consumer, who could do
-    // {...accessor} or
-    // {value, read, write} = accessor
-    // with consequent loss of reactivity and bugs.
+// We opted out from using a `.value` getter+setter,
+// because getter and setters can not be spread using the spread operator (...)
+// and Object.assign() but requires a special extend utility.
+// It would be an easy trap for the API consumer, who could do
+// {...accessor} or
+// {value, read, write} = accessor
+// with consequent loss of reactivity and bugs.
+export function createAccessor<V>(read: () => Promise<V>, write: (value: V) => Promise<V>): AccessorAsync<V>
+export function createAccessor<V>(read: () => V, write: (value: V) => V): AccessorSync<V>
+export function createAccessor<V>(read: () => V, write: (value: V) => V): AccessorSync<V> | AccessorAsync<V> {
     return {read, write}
 }
 
@@ -14,4 +16,9 @@ export function createAccessor<V>(read: () => V, write: (value: V) => V): Access
 export interface AccessorSync<V> {
     read(): V
     write(value: V): V
+}
+
+export interface AccessorAsync<V> {
+    read(): Promise<V>
+    write(value: V): Promise<V>
 }
