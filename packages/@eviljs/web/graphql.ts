29c4f0767872cff6d0e240bb7a56e4b6a7953a24
Daniele Orlando <dnl.rlnd@gmail.com>
2023-11-29 21:00:14 +0100

    (@eviljs/web) graphql handles json response as unknow type

--- packages/@eviljs/web/graphql.ts
+++ packages/@eviljs/web/graphql.ts
@@ -1,5 +1,6 @@
-import {ContentType, HttpMethod} from './fetch.js'
+import {safeObject} from '@eviljs/std/type-safe.js'
 import type {Fetch} from './fetch.js'
+import {ContentType, HttpMethod} from './fetch.js'
 import {asBaseUrl} from './url.js'
 
 export function createGraphql(fetch: Fetch) {
@@ -10,14 +11,14 @@ export function createGraphql(fetch: Fetch) {
     return sendQuery
 }
 
-export function sendGraphqlQuery<T = any>(fetch: Fetch, query: string): Promise<T> {
+export function sendGraphqlQuery<T = unknown>(fetch: Fetch, query: string): Promise<T> {
     const method = HttpMethod.Post
     const url = asBaseUrl(fetch.baseUrl)
     const conf = withRequestGraphql(query)
 
     return fetch.request(method, url, conf)
-        .then(it => it.json())
-        .then(it => it.data as T)
+        .then(it => it.json() as Promise<unknown>)
+        .then(it => safeObject(it).data as T)
 }
 
 export function withRequestGraphql(query: string, variables?: undefined | {}): RequestInit {
