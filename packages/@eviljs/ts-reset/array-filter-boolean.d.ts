98164595e0f1aba1f86aa397a31d41290b616604
Daniele Orlando <dnl.rlnd@gmail.com>
2024-03-20 12:28:22 +0100

    (@eviljs/ts-reset) improves Array.filter(Boolean) typing

--- packages/@eviljs/ts-reset/array-filter-boolean.d.ts
+++ packages/@eviljs/ts-reset/array-filter-boolean.d.ts
@@ -1,6 +1,6 @@
 interface Array<T> {
     filter(predicate: BooleanConstructor, thisArg?: any): Array<
-        T extends undefined | null | false | 0 | 0n | ''
+        T extends void | undefined | null | false | 0 | 0n | ''
             ? never
             : T
     >
@@ -8,7 +8,7 @@ interface Array<T> {
 
 interface ReadonlyArray<T> {
     filter(predicate: BooleanConstructor, thisArg?: any): Array<
-        T extends undefined | null | false | 0 | 0n | ''
+        T extends void | undefined | null | false | 0 | 0n | ''
             ? never
             : T
     >
