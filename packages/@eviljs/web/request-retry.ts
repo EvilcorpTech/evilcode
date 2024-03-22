0829547fd0688d85113cb1fbeb0907cb8fed2891
Daniele Orlando <dnl.rlnd@gmail.com>
2024-03-22 10:42:34 +0100

    (@eviljs/web) request-retry default assert does not reject on bad response

--- packages/@eviljs/web/request-retry.ts
+++ packages/@eviljs/web/request-retry.ts
@@ -2,7 +2,6 @@ import {wait} from '@eviljs/std/async.js'
 import {OneSecondInMs} from '@eviljs/std/date.js'
 import type {Fn, Io} from '@eviljs/std/fn.js'
 import {cloneRequestWithBody} from './request-clone.js'
-import {rejectOnResponseError} from './response.js'
 
 /**
 * @throws
@@ -19,13 +18,16 @@ export function usingFetchRetry(options?: undefined | FetchRetryOptions): Io<Req
 **/
 export function useFetchRetry(request: Request, options?: undefined | FetchRetryOptions): Promise<Response> {
     const executor = options?.fetch ?? fetch
-    const check = options?.check ?? rejectOnResponseError
+    const assert = options?.assert ?? Promise.resolve<Response>
     const times = options?.times ?? 3
     const delay = options?.delay ?? OneSecondInMs
     const delayFactor = options?.delayFactor ?? 3
     const delayMax = options?.delayMax ?? 30_000
     const onError = options?.onError ?? console.error
 
+    /**
+    * @throws
+    **/
     async function retry(error: unknown) {
         if (times === 0) {
             throw error
@@ -44,17 +46,20 @@ export function useFetchRetry(request: Request, options?: undefined | FetchRetry
 
     // We need to clone the request otherwise a TypeError is raised due to used body.
     // `new Request(request)` does not work; we need `request.clone()`.
-    return executor(cloneRequestWithBody(request)).then(check).catch(retry)
+    return executor(cloneRequestWithBody(request)).then(assert).catch(retry)
 }
 
 // Types ///////////////////////////////////////////////////////////////////////
 
 export interface FetchRetryOptions {
-    fetch?: undefined | Io<Request, Promise<Response>>
-    check?: undefined | Io<Response, Promise<Response>>
+    /**
+    * @throws
+    **/
+    assert?: undefined | Io<Response, Promise<Response>>
     delay?: undefined | number // In milliseconds.
     delayFactor?: undefined | number
     delayMax?: undefined | number // In milliseconds.
+    fetch?: undefined | Io<Request, Promise<Response>>
     onError?: undefined | Fn<[error: unknown], void>
     times?: undefined | number
 }
