db316d2b2b0e5ca1a485f95892b1cc4398753a74
Daniele Orlando <dnl.rlnd@gmail.com>
2024-03-24 12:50:32 +0100

    (@eviljs/web) aligns with upstream apis

--- packages/@eviljs/web/throw.ts
+++ packages/@eviljs/web/throw.ts
@@ -3,5 +3,5 @@ import {throwError, StdError} from '@eviljs/std/throw.js'
 export class InvalidResponse extends StdError {}
 
 export function throwInvalidResponse(message?: undefined | string) {
-    return throwError({type: InvalidResponse, message})
+    return throwError(new InvalidResponse(message))
 }
