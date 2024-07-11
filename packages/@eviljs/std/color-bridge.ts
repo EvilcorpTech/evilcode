3bdc814dece2de5c0686b41ae5fac424eb912002
Daniele Orlando <dnl.rlnd@gmail.com>
2024-07-11 17:47:55 +0200

    (@eviljs/std) color has a bridge between hsl and rgb hex

--- packages/@eviljs/std/color-bridge.ts
+++ packages/@eviljs/std/color-bridge.ts
@@ -1,10 +1,14 @@
 import {colorHslFromRgb} from './color-hsl.js'
 import {colorLchFromRgb} from './color-lch.js'
-import {colorRgbFromRgbHexString} from './color-rgb.js'
+import {colorRgbFromHsl, colorRgbFromRgbHexString, colorRgbToRgbHexString} from './color-rgb.js'
 import type {ColorHslDict, ColorLchDict} from './color-types.js'
 
 export function colorHslFromRgbHexString(rgbHexString: string): ColorHslDict {
     return colorHslFromRgb(colorRgbFromRgbHexString(rgbHexString))
+}
+
+export function colorRgbHexStringFromHsl(hsl: ColorHslDict): string {
+    return colorRgbToRgbHexString(colorRgbFromHsl(hsl))
 }
 
 export function colorLchFromRgbHexString(rgbHexString: string): ColorLchDict {
